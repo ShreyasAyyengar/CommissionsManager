c44e6c230776983781764ccadd7f70255ba690dd
Shreyas A <shreyas.ayyengar@gmail.com>
2022-07-04 20:10:39 +0800

    Bugfixes for finding invoice_template.yml in InvoiceDraft#getBaseJSON

--- src/main/java/dev/shreyasayyengar/bot/paypal/InvoiceDraft.java
+++ src/main/java/dev/shreyasayyengar/bot/paypal/InvoiceDraft.java
@@ -46,7 +46,7 @@ public class InvoiceDraft {
     }
 
     private BaseJSON getBaseJSON() throws URISyntaxException, FileNotFoundException {
-        File f = new File(getClass().getClassLoader().getResource("invoice_template.yml").toURI());
+        File f = new File(getClass().getResource("/invoice_template.yml").toURI());
         Yaml yaml = new Yaml();
         Object loadedYaml = yaml.load(new FileReader(f));
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
