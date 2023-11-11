eb35996b85db2d3800a5bb1896e6b76a80a1cb13
Shreyas A <shreyas.ayyengar@gmail.com>
2023-11-11 15:42:00 +0800

    Removed logging of obtaining auth token

--- src/main/java/dev/shreyasayyengar/bot/DiscordBot.java
+++ src/main/java/dev/shreyasayyengar/bot/DiscordBot.java
@@ -68,7 +68,7 @@ public class DiscordBot {
         deserialiseMySQLData();
         initThreadHandler();
 
-        log(Department.Main, "*** CommissionsManager Ready! *** (PRODUCTION-69)");
+        log(Department.Main, "*** CommissionsManager Ready! *** (PRODUCTION-80)");
         System.gc();
     }
 
